d4a3caf4d168af30c403370ce527246139fdf6ef
Eldar <154371617+ELxander7@users.noreply.github.com>
2024-04-20 11:31:46 +0300

    Add files via upload

--- MainTransport.java
+++ MainTransport.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
+import java.sql.SQLOutput;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.time.Instant;
+
 
 public class MainTransport {
     public static void main(String[] args) {
@@ -19,47 +21,70 @@ public class MainTransport {
             TransportDataBase dataBase =
                     mapper.readValue(new File("transport.json"), TransportDataBase.class);
 
-            if (dataBase != null) {
-                dataBase.getData().getVehicles().stream().filter(v -> v.getProperties().getVehicleMetaData().getTransport().getType().equals("bus"));
-            }
-            //1
-            List<String> result1 = dataBase.getData().getVehicles().stream().map(v -> v.getProperties().getVehicleMetaData().getTransport().getType()).distinct().toList();
-            System.out.println(result1);
+            //1. Определить какой транспорт есть в базе данных (троллейбус, автобус...)
+            dataBase.getData().getVehicles().stream().map(vehicle -> vehicle.getProperties().
+                    getVehicleMetaData().getTransport().getType()).distinct().forEach(System.out::println);
 
-            //2
-            List<Vehicle> result2 = dataBase.getData().getVehicles().stream().filter(v -> v.getProperties().getVehicleMetaData().getTransport().getName().equals("55")).toList();
-            System.out.println(result2.size());
 
-            //3
-            Map<String, Integer> map = dataBase.getData().getVehicles().stream().collect(Collectors.toMap(v -> v.getProperties().getVehicleMetaData().getId(), v -> v.getFeatures().stream().mapToInt(f->f.getGeometry().getCoordinates().size()).sum()));
-            System.out.println(map.toString());
+            //2. Определить количество транспорта определенного маршрута на линии
+            System.out.println("Задание 2");
+            System.out.println(dataBase.getData().getVehicles().stream().filter(vehicle -> vehicle.getProperties().
+                    getVehicleMetaData().getTransport().getName().equals("55")).count());
+
+            //3. Создать Map<>  , показывающий транспортное средство и количество сообщений о своих координатах
+            System.out.println("Задание 3");
+            Map<String, Integer> map3 =dataBase.getData().getVehicles().stream().collect(Collectors.
+                    toMap(vehicle -> vehicle.getProperties().getVehicleMetaData().getId(),vehicle -> vehicle.getFeatures().
+                            stream().mapToInt(f -> f.getGeometry().getCoordinates().size()).sum()));
+            System.out.println(map3.toString());
+
+            System.out.println();
+
+            //4. Вывести номер, тип, id транспорта, отсортировав в порядке возрастания номера маршрута
+            System.out.println("Задание 4");
+            dataBase.getData().getVehicles().stream().sorted(Comparator.
+                    comparing(p -> p.getProperties().getVehicleMetaData().getTransport().getName())).distinct().forEach(vehicle -> {
+                    System.out.print(vehicle.getProperties().getVehicleMetaData().getTransport().getName() + " " );
+                    System.out.print(vehicle.getProperties().getVehicleMetaData().getTransport().getType() + " " );
+                    System.out.print(vehicle.getProperties().getVehicleMetaData().getTransport().getId() + " " );
+                    } ) ;
+
+            System.out.println();
+            System.out.println();
+
+            //5. Вывести для каждого транспортного средства массив из времени  (dd.MM.yyyy HH:mm:ss) (Properties)
+            System.out.println("Задание 5");
+            dataBase.getData().getVehicles().stream().flatMapToLong(v -> v.getFeatures().stream()
+                    .mapToLong(e->e.getProperties().getTrajectorySegmentMetaData().getTime())).forEach(num ->
+                    System.out.println(Date.from(Instant.ofEpochSecond(num))));
+
+
+            System.out.println();
+            System.out.println();
+
+            //6. Вывести id только трамваев, автобусов, троллейбусов
+            System.out.println("Задание 6");
+            System.out.println("автобус");
+            dataBase.getData().getVehicles().stream().filter(vehicle -> vehicle.
+                    getProperties().getVehicleMetaData().getTransport().getType().equals("bus")).forEach(vehicle ->
+                    System.out.print(vehicle.getProperties().getVehicleMetaData().getTransport().getId() + " "));
+
+
+            System.out.println();
+            System.out.println("трамвай");
+            dataBase.getData().getVehicles().stream().filter(vehicle -> vehicle.
+                    getProperties().getVehicleMetaData().getTransport().getType().equals("tramway")).forEach(vehicle ->
+                    System.out.print(vehicle.getProperties().getVehicleMetaData().getTransport().getId() + " "));
+
+
+            System.out.println();
+            System.out.println("троллейбус");
+            dataBase.getData().getVehicles().stream().filter(vehicle -> vehicle.
+                    getProperties().getVehicleMetaData().getTransport().getType().equals("trolleybus")).forEach(vehicle ->
+                    System.out.print(vehicle.getProperties().getVehicleMetaData().getTransport().getId() + " "));
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /* public static void findTransportInTransportDataBase(List<Vehicle> vehicles){
-        vehicles.stream().map(vehicle -> vehicle.getProperties().getVehicleMetaData().getTransport().getType().distinct.)
-    } */
